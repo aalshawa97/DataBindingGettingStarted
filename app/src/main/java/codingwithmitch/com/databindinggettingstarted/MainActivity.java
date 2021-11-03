268ad7ecc7b3c06de8fdf607c6e788a3830313b8
aalshawa97 <abdullahalshawa@gmail.com>
2021-11-03 15:30:50 -0700

    Migrated to AndroidX

--- app/src/main/java/codingwithmitch/com/databindinggettingstarted/MainActivity.java
+++ app/src/main/java/codingwithmitch/com/databindinggettingstarted/MainActivity.java
@@ -1,6 +1,6 @@
 package codingwithmitch.com.databindinggettingstarted;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
