600cce0ca78261825bec2081d702cf01a654de35
aalshawa97 <abdullahalshawa@gmail.com>
2021-11-04 11:40:09 -0700

    Set text

--- app/src/main/java/codingwithmitch/com/databindinggettingstarted/adapters/RecyclerAdapter.java
+++ app/src/main/java/codingwithmitch/com/databindinggettingstarted/adapters/RecyclerAdapter.java
@@ -1,29 +1,37 @@
 package codingwithmitch.com.databindinggettingstarted.adapters;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ListView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.*;
 
-import codingwithmitch.com.databindinggettingstarted.MainActivity;
+import codingwithmitch.com.databindinggettingstarted.R;
 import codingwithmitch.com.databindinggettingstarted.models.NicePlace;
 
 public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
-    public RecyclerAdapter(MainActivity mainActivity, ArrayList<NicePlace> nicePlaces) {
+    private List<NicePlace> mNicePlaces = new ArrayList<>();
+    private Context mContext;
+    public RecyclerAdapter(Context context, ArrayList<NicePlace> nicePlaces) {
+        mNicePlaces = nicePlaces;
+        mContext = context;
     }
 
     @NonNull
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+       View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_listitem, parent);
+       return new RecyclerAdapter.ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
-
+        //Set the name of the 'NicePlace'
+       ViewHolder.setText(mNicePlaces.get(position).getTitle());
     }
 
     /**
@@ -34,5 +42,16 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolde
     @Override
     public int getItemCount() {
         return 0;
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        private static String mName;
+        public ViewHolder(View view) {
+            super(view);
+        }
+
+        public static void setText(String aName){
+            mName = aName;
+        }
     }
 }
