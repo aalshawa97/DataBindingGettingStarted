5ff423a0df9b0eb5d507de7b3d09a14fa9bf3548
aalshawa97 <abdullahalshawa@gmail.com>
2021-11-03 18:30:43 -0700

    Toolbar

--- app/src/main/java/codingwithmitch/com/databindinggettingstarted/MainActivity.java
+++ app/src/main/java/codingwithmitch/com/databindinggettingstarted/MainActivity.java
@@ -1,6 +1,8 @@
 package codingwithmitch.com.databindinggettingstarted;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -9,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 }
